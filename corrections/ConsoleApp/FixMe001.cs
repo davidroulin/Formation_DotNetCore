96c83650f2708693593fe6f1ec8988bd8d4560d7
David Roulin <freelance@davidroulin.ch>
2024-08-21 12:14:54 +0200

    projet C# corrections

---
+++ corrections/ConsoleApp/FixMe001.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class FixMe001
+    {
+
+        // What are the differences? Which is better? Which would you rename and why?
+
+        public string HorizontalLine1 = new string('#', 10);
+
+
+        public string HorizontalLine2 => new string('#', 10);
+
+
+        public string HorizontalLine3 { get => new string('#', 10); }
+
+
+        public string HorizontalLine4 { get { return new string('#', 10); } }
+
+
+        public string HorizontalLine5 { get; }
+        public FixMe001()
+        {
+            HorizontalLine5 = new string('#', 10);
+        }
+
+    }
+}
---
+++ corrections/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+
+    // check out https://sharplab.io/
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ca ne fait rien, il faut regarder dans chaque classe.");
+        }
+    }
+}
